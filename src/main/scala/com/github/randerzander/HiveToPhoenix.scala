2facdf194ee61657c7f1e8463c389e8c0eb7a5dd
Randy Gelhausen <rgelhau@gmail.com>
2016-02-03 22:40:19 -0500

    Fixed options, cleaned code, fixed Phoenix->Hive

--- src/main/scala/com/github/randerzander/HiveToPhoenix.scala
+++ src/main/scala/com/github/randerzander/HiveToPhoenix.scala
@@ -5,87 +5,82 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql._
 import java.sql.{Connection, DriverManager, DatabaseMetaData, ResultSet}
 import scala.collection.mutable.HashMap
-import scala.collection.mutable.ArrayBuffer
 import scala.io.Source.fromFile
 
 object HiveToPhoenix{
   def main(args: Array[String]) {
     val props = getProps(args(0))
 
-    val srcTables = props get "srcTables" get
-    val srcScripts = props get "srcScripts" get
+    val srcScripts = props.getOrElse("srcScripts", "").split(",")
+    val srcTables = props.getOrElse("srcTables", "").split(",")
+    val dstTables = props.getOrElse("dstTables", "").split(",").map(t=>t.toUpperCase)
+    val pk = props.getOrElse("dstPk", None)
 
-    val dstUser = props get "dstUser" get
-    val dstPass = props get "dstPass" get
-    val dstClass = props get "dstClass" get
-    val dstConnStr = props get "dstConnStr" get
-    val dstTables = props get "dstTables" get
-    val dstPk = props get "dstPk" get
-    val dstZkUrl = props get "dstZkUrl" get
-    val jars = props get "jars" get
+    val zkUrl = props.getOrElse("zkUrl", "localhost:2181:/hbase-unsecure")
+    val destination = props.getOrElse("destination", "phoenix").toLowerCase()
 
-    val destination = props get "destination" get
-    val dstFormat = props get "dstFormat" get
+    if (srcScripts.size + srcTables.size != dstTables.size){
+      println(srcScripts.size+" srcScripts + "+srcTables.size+" srcTables not equal to "+dstTables.size+" dstTables")
+      System.exit(-1)
+    }
+
+    if (srcScripts.size > 0 && destination.equals("phoenix")){
+      println("SQL scripts for copying from Phoenix to Hive not supported")
+      System.exit(-1)
+    }
+
+    val format = if (destination.equals("phoenix")) "org.apache.phoenix.spark" else props.getOrElse("format", "orc")
+    val jdbcClass = "org.apache.phoenix.jdbc.PhoenixDriver"
+    val connStr = "jdbc:phoenix:" + zkUrl
+    val jars = props.getOrElse("jars", "").split(",")
 
     // Establish src->dst type mapping
     var typeMap = new HashMap[String, String]().withDefaultValue(null)
-    props.get("typeMap").get.split(",")
+    props.getOrElse("typeMap", "").split(",")
       .map(x => typeMap.put(x.split("\\|")(0).toLowerCase, x.split("\\|")(1).toLowerCase))
 
     // Create SparkContext
-    var sparkConf = new SparkConf().setAppName("HiveToPhoenix")
-    if (jars != null) sparkConf = sparkConf.setJars(jars.split(","))
-    val sc = new SparkContext(sparkConf)
+    val sparkConf = new SparkConf().setAppName("HiveToPhoenix")
+    val sc = new SparkContext(if (jars.size > 0) sparkConf.setJars(jars) else sparkConf)
     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
 
-    // Build array of queries for generating DataFrames
-    var queries = ArrayBuffer[String]()
-    if (srcScripts != null)
-      for (script <- srcScripts.split(",")) queries += fromFile(script).getLines().mkString("")
-    if (srcTables != null)
-      for (table <- srcTables.split(",")) queries += "select * from " + table
+    // Build array of DataFrames for saving
+    var dfs = Array[DataFrame]()
+    val scripts = srcScripts.map(script => fromFile(script).getLines().mkString(""))
+    scripts.map(q => dfs = dfs :+ sqlContext.sql(q.stripSuffix(";")))
+    srcTables.map(t =>
+      if (destination.equals("phoenix")) dfs = dfs :+ sqlContext.sql("select * from " + t)
+      else dfs = dfs :+ sqlContext.load(format, Map("table" -> t, "zkUrl" -> zkUrl))
+    )
 
-    for ((query, i) <- queries.zipWithIndex) {
-      println("INFO: SparkSQL query: " + query.stripSuffix(";"))
-      var df = sqlContext.sql(query.stripSuffix(";"))
-      df = df.toDF(df.columns.map(x => x.toUpperCase): _*)
-      val dstTable = dstTables.split(",")(i).toUpperCase
-
-      if (destination.toLowerCase.equals("phoenix")){
+    var queries = scripts :+ srcTables.map(t => "select * from " + t)
+    for((df, i) <- dfs.zipWithIndex){
+      println("INFO: Saving query ("+queries(i)+") to "+destination+" table: " + dstTables(i))
+      val tmpDf = df.toDF(df.columns.map(x => x.toUpperCase): _*)
+      if (destination.equals("phoenix")){
         // Create DDL
-        var command = "create table if not exists " + dstTable + "("
+        var command = "create table if not exists " + dstTables(i) + "("
         for (field <- df.schema) {
           val srcType = field.dataType.simpleString
-          val dstType = if (typeMap contains srcType) typeMap get srcType get else srcType
+          val dstType = typeMap.getOrElse(srcType, srcType)
           command += field.name + " " + dstType + ","
         }
-        command += " constraint my_pk primary key (" + dstPk + "))"
+        command += " constraint my_pk primary key ("+pk+"))"
         println("INFO: DESTINATION DDL:\n" + command)
         // Execute Phoenix DDL
-        getConn(dstClass, dstConnStr, dstUser, dstPass).createStatement().execute(command)
-
-        // Save query results in Phoenix
-        println("INFO: Saving to Phoenix!")
-        df.save(
-          "org.apache.phoenix.spark",
-          SaveMode.Overwrite,
-          Map("table" -> dstTable, "zkUrl" -> dstZkUrl)
-        )
+        getConn(jdbcClass, connStr).createStatement().execute(command)
       }
-      else {
-        println("INFO: Saving to Hive!")
-        df.write.format(dstFormat).saveAsTable(dstTable)
-      }
+      tmpDf.write.format(format).mode(SaveMode.Overwrite).options(Map("table" -> dstTables(i), "zkUrl" -> zkUrl)).save()
     }
 
     sc.stop()
   }
 
-  def getConn(driverClass: => String, connStr: => String, user: => String, pass: => String): Connection = {
+  def getConn(driverClass: => String, connStr: => String): Connection = {
     var conn:Connection = null
     try{
       Class.forName(driverClass)
-      conn = DriverManager.getConnection(connStr, user, pass)
+      conn = DriverManager.getConnection(connStr, "", "")
     }catch{
       case e: Exception => {
         e.printStackTrace
